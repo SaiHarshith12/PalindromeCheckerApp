11c7ece15c61cfb6ff64b3c065c92cc9de66edaf
SaiHarshith12 <varanasisaiharshith@gmail.com>
2026-02-16 11:05:24 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+/*
+This is a management system created by me.It basically checks if it is a palindrome or not.
+@author SaiHarshith12
+@version 1.0
+ */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to my Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
