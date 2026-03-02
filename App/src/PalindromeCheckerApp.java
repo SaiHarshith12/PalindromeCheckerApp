97dfbac37dea47c74bf818f4d8613f7736302fe7
SaiHarshith12 <varanasisaiharshith@gmail.com>
2026-03-02 09:47:00 +0530

    SaiHarshith12 Added UC11

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
-public class PalindromeCheckerApp {
-        public static boolean isPalindrome(String str) {
-            str = str.toLowerCase().replace(" ", "");
-            int left = 0;
-            int right = str.length() - 1;
+class palindrome {
+    private String cleanString(String input) {
+        return input.toLowerCase().replaceAll("[^a-z0-9]", "");
+    }
 
-            while (left < right) {
-                if (str.charAt(left) != str.charAt(right)) {
-                    return false;
-                }
-                left++;
-                right--;
+    public boolean isPalindrome(String input) {
+        String str = cleanString(input);
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
             }
-
-            return true;
+            left++;
+            right--;
         }
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a string: ");
-            String input = scanner.nextLine();  // nextLine to allow spaces
+        return true;
+    }
+}
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
 
-            if (isPalindrome(input)) {
+            Scanner scanner = new Scanner(System.in);
+            palindrome service = new palindrome();
+
+            System.out.print("Enter a string to be checked: ");
+            String input = scanner.nextLine();
+
+            boolean result = service.isPalindrome(input);
+
+            if (result) {
                 System.out.println("Palindrome");
             } else {
                 System.out.println("Not a Palindrome");
