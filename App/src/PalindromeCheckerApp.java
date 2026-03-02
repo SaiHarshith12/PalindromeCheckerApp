61989c94827ff46683a35369075be832dfb8a1fd
SaiHarshith12 <varanasisaiharshith@gmail.com>
2026-03-02 09:08:15 +0530

    SaiHarshith12 Added UC6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 public class PalindromeCheckerApp {
         public static void main(String[] args) {
+
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter a string: ");
             String str = sc.nextLine();
+
             Stack<Character> stack = new Stack<>();
+            Queue<Character> queue = new LinkedList<>();
             for (int i = 0; i < str.length(); i++) {
                 stack.push(str.charAt(i));
+                queue.add(str.charAt(i));
             }
             boolean isPalindrome = true;
             for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) != stack.pop()) {
+                if (!stack.pop().equals(queue.remove())) {
                     isPalindrome = false;
                     break;
                 }
